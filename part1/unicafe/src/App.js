ca70091e28ccf7657cbff989a4b102877939568a
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-05-06 17:40:37 -0700

    Finish exercise 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,15 +1,26 @@
 import { useState } from 'react'
 
-const Header = () => <h1>give feedback</h1>
+const Header = ({title}) => <h1>{title}</h1>
 
 const App = () => {
     const [ good, setGood ] = useState(0)
     const [ neutral, setNeutral ] = useState(0)
     const [ bad, setBad ] = useState(0)
 
+    const incrementStateHandler = (state, stateSetter) => () => {
+        stateSetter(state + 1)
+    }
+
     return (
         <div>
-            <Header />
+            <Header title="give feedback" />
+            <button onClick={incrementStateHandler(good, setGood)}>good</button>
+            <button onClick={incrementStateHandler(neutral, setNeutral)}>neutral</button>
+            <button onClick={incrementStateHandler(bad, setBad)}>bad</button>
+            <Header title="statistics" />
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
         </div>
     )
 }
