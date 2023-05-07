97b6e61a44ed2489de2331b835c3e9662f3702ef
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-05-06 17:50:08 -0700

    Finish exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,6 +2,24 @@ import { useState } from 'react'
 
 const Header = ({title}) => <h1>{title}</h1>
 
+const Statistics = ({good, neutral, bad}) => {
+    const numFeedback = () => good + neutral + bad
+    const averageFeedback = () => (good-bad)/numFeedback()
+    const percentPositive = () => 100*good/numFeedback()
+
+    return (
+        <>
+            <Header title="statistics" />
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <p>all {numFeedback()}</p>
+            <p>average {averageFeedback()}</p>
+            <p>positive {percentPositive()}%</p>
+        </>
+    )
+}
+
 const App = () => {
     const [ good, setGood ] = useState(0)
     const [ neutral, setNeutral ] = useState(0)
@@ -11,23 +29,13 @@ const App = () => {
         stateSetter(state + 1)
     }
 
-    const numFeedback = () => good + neutral + bad
-    const averageFeedback = () => (good-bad)/numFeedback()
-    const percentPositive = () => 100*good/numFeedback()
-
     return (
         <div>
             <Header title="give feedback" />
             <button onClick={incrementStateHandler(good, setGood)}>good</button>
             <button onClick={incrementStateHandler(neutral, setNeutral)}>neutral</button>
             <button onClick={incrementStateHandler(bad, setBad)}>bad</button>
-            <Header title="statistics" />
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {numFeedback()}</p>
-            <p>average {averageFeedback()}</p>
-            <p>positive {percentPositive()}%</p>
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
