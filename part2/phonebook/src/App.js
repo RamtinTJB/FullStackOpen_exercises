64cca64f5e0eea7be72eb21bffa62735e0212c9b
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-05-16 18:04:51 -0700

    Add new names to the phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,36 @@
+import { useState } from 'react'
+
 const App = () => {
-    return <h1>Hello World</h1>
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ])
+    const [newName, setNewName] = useState('')
+    
+    const addButtonHandler = (event) => {
+        event.preventDefault()
+        const copy = persons.concat({ name: newName })
+        setPersons(copy)
+    }
+
+    const nameChangeHandler = (event) => {
+        setNewName(event.target.value)
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form>
+                <div>
+                    name: <input value={newName} onChange={nameChangeHandler}/>
+                </div>
+                <div>
+                    <button type="submit" onClick={addButtonHandler}>add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map(person => <p key={person.name}>{person.name}</p>)}
+        </div>
+    )
 }
 
 export default App
