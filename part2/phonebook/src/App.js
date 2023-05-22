4a46f01af4fecec648e566a9f8d47e328638268c
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-05-22 07:37:05 +0300

    Show contacts in a table

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,3 @@
-import axios from 'axios'
 import { useEffect, useState } from 'react'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,14 +1,15 @@
 const Persons = ({ persons, deleteHandler }) => {
     return (
-        <>
+        <table>
             {persons.map(person => 
-                <div key={person.id}>
-                    {person.name} {person.number}
-                    <button onClick={() => deleteHandler(person.id)}>delete</button>
-                </div>
+                <tr key={person.id}>
+                    <td>{person.name}</td>
+                    <td>{person.number}</td>
+                    <td><button onClick={() => deleteHandler(person.id)}>delete</button></td>
+                </tr>
             )
             }
-        </>
+        </table>
     )
 }
 
