13f93dcd0544601c2e4ff87d376041553ae81a41
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-05-30 21:14:14 -0700

    Deploy app to fly server

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -15,6 +15,7 @@ morgan.token('post-content', (req, res) => {
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-content'))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
