6b983209d1debb34aa919d4f099416623bd7123f
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-06-03 16:18:59 -0700

    Update existing person and display info from MongoDB

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -19,29 +19,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
 app.use(cors())
 app.use(express.static('build'))
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(result => {
         response.json(result)
@@ -50,8 +27,6 @@ app.get('/api/persons', (request, response, next) => {
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    const id = Number(request.params.id)
-
     Person.findById(request.params.id).then(person => {
         if (person) {
             response.json(person)
@@ -70,10 +45,6 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-const getRandomInt = (max) => {
-    return Math.floor(Math.random() * max)
-}
-
 const returnError = (response, msg) => {
     return response.status(400).json({
         error: msg
@@ -87,10 +58,6 @@ app.post('/api/persons', (request, response) => {
         return returnError(response, "Name or number is missing")
     }
 
-    // if (persons.filter(p => p.name === person.name).length > 0) {
-    //     return returnError(response, "Name must be unique")
-    // }
-
     const person = new Person({
         name: body.name,
         number: body.number,
@@ -101,8 +68,30 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people<br/>${Date()}`)
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    if (!('name' in body) || !('number' in body)) {
+        return returnError(response, "Name or number is missing")
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+app.get('/info', (request, response, error) => {
+    Person.find({}).then(result => {
+        response.send(`Phonebook has info for ${result.length} people<br/>${Date()}`)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, repsonse) => {
