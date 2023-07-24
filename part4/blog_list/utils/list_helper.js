85db44d3433a2159ca4009a118af925e81071d45
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2023-07-24 15:21:55 -0700

    Add favoriteBlog function with test

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -8,7 +8,22 @@ const totalLikes = (blogs) => {
         : blogs.reduce((acc, curr) => acc + curr.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return {}
+
+    const favBlog = blogs.reduce((prev, curr) => {
+        return prev.likes > curr.likes ? prev : curr
+    })
+
+    return {
+        title: favBlog.title,
+        author: favBlog.author,
+        likes: favBlog.likes
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
